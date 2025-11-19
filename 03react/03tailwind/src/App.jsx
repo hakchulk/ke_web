c7ba9bbf06dd6fbadaa04e0ee622787354d72c77
hakchulk <hakchulk@gmail.com>
2025-11-19 12:46:46 +0900

    OpenWeatherMap API_KEY  삭제

--- 03react/03tailwind/src/App.jsx
+++ 03react/03tailwind/src/App.jsx
@@ -1,76 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [boadData, setBoadData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get("http://localhost:8080/api/board");
+      console.log("fetchData() data:", data);
+      setBoadData(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div className="bg-gray-200 w-full h-screen flex justify-center items-center">
-        <div className=" w-[70%] sm:w-120 rounded-2xl border-gray-200 shadow-xl p-8">
-          <div className="mb-6">
-            <h2 className="text-2xl font-bold mb-2">로그인</h2>
-            <p className="text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
-            </p>
+    <div>
+      {boadData &&
+        boadData.map((item) => (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.name}</p>
+            <p>{item.content}</p>
+            <hr />
           </div>
-          <div className="mb-6">
-            <button className="flex w-full bg-blue-200 p-2 rounded border-gray-300 text-sm justify-center items-center mb-4 hover:bg-blue-300 gap-2">
-              <svg className="size-5" viewBox="0 0 48 48">
-                <path
-                  fill="#FFC107"
-                  d="M43.611 20.083H42V20H24v8h11.303C33.915 31.658 29.323 35 24 35c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.869 5.139 29.748 3 24 3 12.954 3 4 11.954 4 23s8.954 20 20 20 20-8.954 20-20c0-1.341-.138-2.651-.389-3.917z"
-                />
-                <path
-                  fill="#FF3D00"
-                  d="M6.306 14.691l6.571 4.818C14.297 15.097 18.789 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.869 5.139 29.748 3 24 3 16.318 3 9.656 7.337 6.306 14.691z"
-                />
-                <path
-                  fill="#4CAF50"
-                  d="M24 43c5.251 0 10.053-2.007 13.682-5.289l-6.312-5.338C29.358 33.798 26.846 35 24 35c-5.298 0-9.874-3.317-11.289-7.946l-6.55 5.046C9.472 39.556 16.178 43 24 43z"
-                />
-                <path
-                  fill="#1976D2"
-                  d="M43.611 20.083H42V20H24v8h11.303a11.996 11.996 0 01-4.089 5.662l.003-.002 6.312 5.338C36.94 39.229 44 35 44 23c0-1.341-.138-2.651-.389-3.917z"
-                />
-              </svg>
-              구글 로그인
-            </button>
-          </div>
-          <div className="mb-6">
-            <form action="" className="mb-4">
-              <div className="mb-2">
-                <label htmlFor="email" className="block text-sm mb-2">
-                  이메일
-                </label>
-                <input
-                  type="text"
-                  id="email"
-                  className="w-full border border-gray-300 rounded-lg p-2"
-                  placeholder=" "
-                />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="pdw" className="block text-sm mb-2">
-                  password
-                </label>
-                <input
-                  type="text"
-                  id="pwd"
-                  className="w-full border border-gray-300 rounded-lg p-2"
-                  placeholder=" "
-                />
-              </div>
-              <button className="flex justify-center items-center gap-3 w-full rounded-lg bg-info text-white py-2 hover:bg-blue-600 transition">
-                <div className="w-5 h-5 bg-amber-400 animate-spin rounded-[2px]"></div>
-                로그인
-              </button>
-            </form>
-            <p className="text-sm text-center text-gray-500">
-              {" "}
-              Lorem ipsum dolor sit amet.
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
+        ))}
+    </div>
   );
 }
 
